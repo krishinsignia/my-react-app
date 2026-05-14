248c3491b30f853777813524f1ffabb206c8e2f1
krishinsignia <krish.insignia@gmail.com>
2026-05-14 10:38:49 +0530

    Added navbar section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to My React App 🚀</h1>
+
+      <p>This is my first React application.</p>
+
+      <button
+        style={{
+          padding: "12px 20px",
+          fontSize: "18px",
+          backgroundColor: "blue",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 }
